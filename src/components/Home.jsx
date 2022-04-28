29fb6a29b7c665b694a39f98f986bea55ecb1939
ifeoluwa oladeni <oladeniifeoluwa123@gmail.com>
2022-04-28 11:17:46 +0100

    E-commerce

---
+++ src/components/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Product from './Product'
+
+const Home = () => {
+    return (
+        <div className='hero'>
+            <div class="card bg-dark text-white border -0">
+                <img src="/assets/bg.jpeg" class="card-img" alt="background" height="550px"></img>
+                    <div class="card-img-overlay d-flex  flex-column">
+                    <div className='container'>
+                        <h5 class="card-title display-3 fw-bolder mb-0"> NEW ARRIVAL SEASON</h5>
+                        <p class="card-text lead fs-2">CHECK OUT ALL THE NEW TRENDS</p>
+                        </div>
+                    </div>
+            </div>
+            <Product />
+        </div>
+    )
+}
+
+export default Home
