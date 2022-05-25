b10f16ff078632ebeaa648bfbb711a2eef0e9df2
ifeoluwa oladeni <oladeniifeoluwa123@gmail.com>
2022-05-26 00:01:05 +0100

    working on cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -69,6 +69,7 @@ const Cart = () => {
                         </div>
                       </div>
                     </div>
+                    <p>Work in progress</p>
                   </div>
             </React.Fragment>
       )
